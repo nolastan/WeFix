9ae5ab89a79c31e66748e627ad77018397b8ab5c
Stanford Rosenthal <nolastan@gmail.com>
2012-04-25 17:46:45 -0500

    ...

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,35 +3,8 @@ class RequestsController < ApplicationController
     @requests = Request.where(:status => "open").where("media_url IS NOT NULL").order("requested_datetime DESC")
   end
 
-  def show
-    require "uri"
-    require "net/http"
-    
+  def show    
     @request = Request.find(params[:id])
-    url = ENV["tr_url"] || APP_CONFIG['taskrabbit'][:url]
-    params = {
-      "task" => {
-        "name" => "Dog walking3",
-        "named_price" => 20, 
-        "city_id" => 1,
-        "description" => "I want you to walk him to his favorite state park",
-        "cost_in_cents" => 500,
-        "virtual" => true
-      }
-    }
-    # @post_url = URI.parse(url + "tasks.php?access_token=" + User.find(cookies[:user_id]).token)
-    @post_url = "http://stanfordrosenthal.com/wefix/referer.php"
-    print "\n"
-    print "\n"
-    print "\n"
-    print @post_url
-    print "\n"
-    print Net::HTTP::Post.new(@post_url.path)
-    print "\n"
-    print Net::HTTP::Post.new(@post_url.path).body
-    print "\n"
-    print "\n"
-    print "\n"
   end
   
   def remote_post
