409ee7e389765b20e54412eaaa8e92f133bce3f5
Stanford Rosenthal <nolastan@gmail.com>
2012-04-25 00:33:01 -0500

    asdf

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -13,6 +13,8 @@ class RequestsController < ApplicationController
     require "net/http"
     url =  ENV["tr_url"] || APP_CONFIG['taskrabbit'][:url]
 
+    print url
+
     post_url = URI.parse(url + "tasks.php?access_token=" + User.find(cookies[:user_id]).token)
     params = {
       "task" => {
@@ -27,6 +29,7 @@ class RequestsController < ApplicationController
     
     # @request = Request.find(params[:id])
     # print "POSTING TO " + post_url + "tasks?access_token=" + User.find(cookies[:user_id]).token
+    print post_url
     print Net::HTTP.post_form(post_url, params)
     print "\n\n --- END POST --- \n\n"
   end
