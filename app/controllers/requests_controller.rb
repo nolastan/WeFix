23f9db60c5475fcc02295fecf292cf231f21c6d0
Stanford Rosenthal <nolastan@gmail.com>
2012-04-25 01:00:44 -0500

    trying

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,18 +4,11 @@ class RequestsController < ApplicationController
   end
 
   def show
-    @request = Request.find(params[:id])
-    
-  end
-  
-  def remote_post
     require "uri"
     require "net/http"
-    url =  ENV["tr_url"] || APP_CONFIG['taskrabbit'][:url]
-
-    print url
-
-    post_url = URI.parse(url + "tasks.php?access_token=" + User.find(cookies[:user_id]).token)
+    
+    @request = Request.find(params[:id])
+    url = ENV["tr_url"] || APP_CONFIG['taskrabbit'][:url]
     params = {
       "task" => {
         "name" => "Dog walking3",
@@ -26,11 +19,23 @@ class RequestsController < ApplicationController
         "virtual" => true
       }
     }
+    @post_url = URI.parse(url + "tasks.php?access_token=" + User.find(cookies[:user_id]).token)
+    print @post_url
+    print Net::HTTP.post_form(@post_url, params).body
+  end
+  
+  def remote_post
+
+    url =  ENV["tr_url"] || APP_CONFIG['taskrabbit'][:url]
+
+    print url
+
+    post_url = URI.parse(url + "tasks.php?access_token=" + User.find(cookies[:user_id]).token)
+
     
     # @request = Request.find(params[:id])
     # print "POSTING TO " + post_url + "tasks?access_token=" + User.find(cookies[:user_id]).token
     print post_url
-    print Net::HTTP.post_form(post_url, params)
     print "\n\n --- END POST --- \n\n"
   end
   
