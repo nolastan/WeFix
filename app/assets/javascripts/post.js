e3de1bf93d94e613f88171ee5767c576fa514056
Stanford Rosenthal <nolastan@gmail.com>
2012-04-25 01:48:54 -0500

    trying jquery

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
   $('#tr_post').click(function() {
 
-      var postTo = 'http://sandbox-wefix0001.taskrabbitdev.com/api/v1/tasks?access_token=aDZaYIrNpouGs4M6dGI1xMPoTJ1ru15nGfizPoGv';
+      var postTo = 'http://sandbox-wefix0001.taskrabbitdev.com/api/v1/tasks?access_token=x7Hf13HhWwwzB2Iu7Kx7hPhsN18R4YPimKYO0b2v';
 
       $.post(postTo,({name: "Walk doggy", named_price: "20", city_id: "1"}),
       function(data) {
